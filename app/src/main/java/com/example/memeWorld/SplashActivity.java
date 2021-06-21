18a1e844163b6b1f39c1cf22a89b3c89683f6cf4
Rishav <rishavsharma1999@gmail.com>
2021-06-22 00:14:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memeWorld/SplashActivity.java
@@ -0,0 +1,24 @@
+
+package com.example.memeWorld;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 3000);
+    }
+}
